d41eb15bf93d59b2616913752df860c0d73bb8f2
projectAmazon <lm1193737@gmail.com>
2024-02-25 03:49:02 -0800

    save and generarte html for deleviry options

---
+++ data/deleviryOptions.js
@@ -0,0 +1,13 @@
+export const deleviryOptions = [{
+    id:'1',
+    deleviryDate:7,
+    priceCents:0
+},{
+    id:'2',
+    deleviryDate:3,
+    priceCents:499
+},{
+    id:'3',
+    deleviryDate:1,
+    priceCents:999
+}];
