436ad268b46b0bf230fd8c3a68e4632b0c6044b8
projectAmazon <lm1193737@gmail.com>
2024-03-02 03:15:26 -0800

    trying to make search box interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -3,102 +3,117 @@ import {products} from '../data/products.js';
 
 
 
-let productHtml = '';
+function allProducts (){
 
-products.forEach((product) => {
-    productHtml += `
-    <div class="product-container">
-          <div class="product-image-container">
-            <img class="product-image"
-              src="${product.image}">
-          </div>
+    let productHtml = '';
 
-          <div class="product-name limit-text-to-2-lines">
-            ${product.name}
-          </div>
-
-          <div class="product-rating-container">
-            <img class="product-rating-stars"
-              src="images/ratings/rating-${product.rating.stars * 10}.png">
-            <div class="product-rating-count link-primary">
-              ${product.rating.count}
+  products.forEach((product) => {
+      productHtml += `
+      <div class="product-container">
+            <div class="product-image-container">
+              <img class="product-image"
+                src="${product.image}">
             </div>
-          </div>
 
-          <div class="product-price">
-           ₹${product.priceCents}
-          </div>
+            <div class="product-name limit-text-to-2-lines">
+              ${product.name}
+            </div>
 
-          <div class="product-quantity-container">
-            <select class="js-quantity-selector-${product.id}">
-              <option selected value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-              <option value="10">10</option>
-            </select>
-          </div>
+            <div class="product-rating-container">
+              <img class="product-rating-stars"
+                src="images/ratings/rating-${product.rating.stars * 10}.png">
+              <div class="product-rating-count link-primary">
+                ${product.rating.count}
+              </div>
+            </div>
 
-          <div class="product-spacer"></div>
+            <div class="product-price">
+            ₹${product.priceCents}
+            </div>
 
-          <div class="added-to-cart js-added-tick-${product.id}">
-            <img src="images/icons/checkmark.png">
-            Added
-          </div>
+            <div class="product-quantity-container">
+              <select class="js-quantity-selector-${product.id}">
+                <option selected value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+              </select>
+            </div>
 
-          <button class="add-to-cart-button button-primary js-add-to-cart" 
-          data-product-id ="${product.id}">
-            Add to Cart
-          </button>
-    </div>
-    `;
-});
+            <div class="product-spacer"></div>
 
-document.querySelector('.js-products-grid').innerHTML = productHtml;
+            <div class="added-to-cart js-added-tick-${product.id}">
+              <img src="images/icons/checkmark.png">
+              Added
+            </div>
 
-function updateCartQuantity(){
+            <button class="add-to-cart-button button-primary js-add-to-cart" 
+            data-product-id ="${product.id}">
+              Add to Cart
+            </button>
+      </div>
+      `;
+  });
+
+  document.querySelector('.js-products-grid').innerHTML = productHtml;
+
+  function updateCartQuantity(){
+        
+      let cartQuantity = 0;
+
+      cart.forEach((cartItem) => {
+          cartQuantity += cartItem.quantity;
+      });
+
+      document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+  };
+
+  updateCartQuantity();
+
+  document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+      button.addEventListener('click', () => {
+          const productId = button.dataset.
+          productId;
+
+          const qunatitySelector = document.querySelector(`.js-quantity-selector-${productId}`) ;
+
+          const quantity =Number(qunatitySelector.value);
+
+          const addedMessge = document.querySelector(`.js-added-tick-${productId}`) ;
+
+          addedMessge.classList.add('added-to-cart-visible');
+
+          setTimeout(() =>{
+            addedMessge.classList.remove('added-to-cart-visible');
+          },2000);
+
+
+          addToCart(productId,quantity);
+          updateCartQuantity();
+
+        
+      });
       
-    let cartQuantity = 0;
 
-    cart.forEach((cartItem) => {
-        cartQuantity += cartItem.quantity;
-    });
+  });
 
-    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
 };
 
-updateCartQuantity();
-
-document.querySelectorAll('.js-add-to-cart').forEach((button) => {
-    button.addEventListener('click', () => {
-        const productId = button.dataset.
-        productId;
-
-        const qunatitySelector = document.querySelector(`.js-quantity-selector-${productId}`) ;
-
-        const quantity =Number(qunatitySelector.value);
-
-        const addedMessge = document.querySelector(`.js-added-tick-${productId}`) ;
-
-        addedMessge.classList.add('added-to-cart-visible');
-
-        setTimeout(() =>{
-          addedMessge.classList.remove('added-to-cart-visible');
-        },2000);
+allProducts();
 
 
-        addToCart(productId,quantity);
-        updateCartQuantity();
 
-       
-    });
-    
 
+document.querySelector('.js-search-button').addEventListener('click', () => {
+  const searchResult = document.querySelector('.js-search-input-box').value;
+
+  console.log(searchResult);
 });
-
 
