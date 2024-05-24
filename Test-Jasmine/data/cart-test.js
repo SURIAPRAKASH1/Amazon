9cb1221a5ef614431e5740409bcbd8dee027dd39
projectAmazon <lm1193737@gmail.com>
2024-05-24 04:50:10 -0700

    call it,s test

---
+++ Test-Jasmine/data/cart-test.js
@@ -0,0 +1,12 @@
+import { addToCart, cart } from "../../data/cart.js";
+
+describe('test suite: addtocart', () => {
+    it('adds an exting product to cart', () => {
+
+    });
+
+    it('adds new product to cart', () => {
+        addToCart("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
+        expect(cart.length).toEqual(1);
+    });
+})
