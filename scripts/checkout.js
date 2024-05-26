c9ba09319b76773b6817e7c9839079c49470c683
projectAmazon <lm1193737@gmail.com>
2024-05-26 03:59:17 -0700

    practice promise

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -5,6 +5,17 @@ import { renderPaymentSummary } from './checkout/paymentSummary.js';
 //import '../data/cart-class.js'
 
 
+/*new Promise((resolve) => {
+    console.log('start loading')
+    const is_perfect = true
+
+    if (is_perfect) {
+        console.log("some thing");
+        resolve();
+    }
+}).then(() => console.log("next step")) 
+*/
+
 renderOrderSummary();
 renderPaymentSummary();
 
